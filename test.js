42b7467410a623d04916473d7c492f90a30df261
Sarthak Bhattarai <sarthakbhattarai521@gmail.com>
2025-05-17 18:45:38 +0545

    javascript1

---
+++ test.js
@@ -0,0 +1,28 @@
+let i=0;
+for(i=0;i<=5;i++){
+    console.log(i);
+}
+
+let age=19;
+if(age<18){
+    console.log("You cannot vote");
+}
+else{
+    console.log("You can vote");
+}
+
+
+let array=['sarthak', 'sabal', 'rahul'];
+
+for (let j=0;j<array.length;j++){
+    console.log(array[j]);
+}
+
+function add(a,b){
+    return a+b;
+}
+
+console.log(add(2,3));
+
+
+
